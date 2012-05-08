21e9f872fccbebd74e60f83cdf373806d2c8a02e
jec1477 <jec1477@gmail.com>
2012-05-08 09:20:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Klife::Application.config.secret_token = '1671e93d2978bb3a6f822b59def16d56b0cd50c7b8f2526ecb3f762d8ed20e694533ffa227fa1ddc71d608913f7c8e4e61c312cd7f5e3c29bac9e56dc2b86942'
