da953d4fcabfa644380dffb4e25f878b512eeb04
jec1477 <jec1477@gmail.com>
2012-05-08 20:03:16 -0400

    routes for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Klife::Application.routes.draw do
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'resources#index'
+
   resources :resources
 
   # The priority is based upon order of creation:
@@ -47,10 +52,6 @@ Klife::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
